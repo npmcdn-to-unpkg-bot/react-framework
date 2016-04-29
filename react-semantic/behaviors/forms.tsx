3b2f5365345dcae7eded5174ffe957d2456de38d
PZ <pzika@langmaster.cz>
2016-04-29 12:49:24 +0200

    v

--- react-semantic/behaviors/forms.tsx
+++ react-semantic/behaviors/forms.tsx
@@ -8,20 +8,17 @@ import * as flux from '../../react-framework/flux';
 
 const moduleId = 'forms-ui';
 
-export class FormSmart extends flux.Component<FormSmartStore, flux.IPropsEx & ui.FormProps> { getChildContext() { return { MyForm: this.state }; } }
-FormSmart['childContextTypes'] = { MyForm: React.PropTypes.any };
+export class FormSmart extends forms.FormLow<FormSmartStore, ui.FormProps> {}
 
 @flux.StoreDef({ moduleId: moduleId, componentClass: FormSmart })
-export class FormSmartStore extends forms.FormStore {
+export class FormSmartStore extends forms.FormLowStore {
   render(): JSX.Element { return React.createElement(ui.Form, this as any, this.children); }
 }
 
-export class FieldSmart extends flux.Component<FieldSmartStore, forms.InputProps & ui.FieldProps> {  getChildContext() { return { MyInput: this.state }; } }
-FieldSmart['childContextTypes'] = { MyInput: React.PropTypes.any };
-FieldSmart['contextTypes'] = { MyForm: React.PropTypes.any };
+export class FieldSmart extends forms.InputLow<FieldSmartStore, ui.FieldProps> { }
 
 @flux.StoreDef({ moduleId: moduleId, componentClass: FieldSmart })
-export class FieldSmartStore extends forms.InputStore {
+export class FieldSmartStore extends forms.InputLowStore {
   render(): JSX.Element {
     var props: ui.FieldProps = Object.assign({}, this); 
     props.$error = !!this.error;
